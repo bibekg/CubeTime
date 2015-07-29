b0c30db2a6028b94e5df631f90a2610a3ac76571
Bibek <ghim.bibek@gmail.com>
2015-07-29 00:06:55 -0700

    Initial Commit

---
+++ CubeTime/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CubeTime
+//
+//  Created by Bibek Ghimire on 7/29/15.
+//  Copyright (c) 2015 Bibek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CubeTime/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CubeTime
+//
+//  Created by Bibek Ghimire on 7/29/15.
+//  Copyright (c) 2015 Bibek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
